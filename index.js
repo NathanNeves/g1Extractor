6399980876429f4cdd452660f464310e54170594
Nathan Aguiar <nathann-s@outlook.com>
2020-01-10 18:26:07 -0300

    Finalização da api

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
-const fs = require('fs');
+const fs = require('fs-extra');
+const homofobia = require('./Racismo.json');
+
 
 async function takeData(termo){
-    try{
+        let noticias = new Array();
         for(let i=2007;i<2021;i++){    
                 for(let n = 1;n<36;n++){
                 let url = `https://g1.globo.com/busca/?q=${termo}&order=recent&from=${i}-01-01T00%3A00%3A00-0300&to=${i}-12-31T23%3A59%3A59-0300&page=${n}&ajax=1`;
+                try{
                 let html =  await  axios.get(url);
                 const $ = cheerio.load(html.data);
                 if($('.widget--no-results').length){
@@ -14,14 +17,19 @@ async function takeData(termo){
                 }
                 $('.widget--info__title').each((i,elem)=>{
                     console.log(elem.children[0].data);
+                    noticias.push({titulo:elem.children[0].data.replace(/(\r\n|\n|\r|\\n)/gm, '').trim(),classificao:'undefined'});
                 })  
+            }catch(e){
+                console.log(e);
+                break;
             }
+            
         }
-
-    }catch(e){
-        console.log(e);
     }
+    await fs.writeJson(`${termo}.json`,noticias);
+
 
 }
 
-takeData('homofobia');
+console.log(homofobia);
+
