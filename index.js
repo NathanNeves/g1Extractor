000484af46897a8678aca05e6715585433613a52
Nathan Aguiar <nathann-s@outlook.com>
2020-10-22 23:47:22 -0300

    Refactor no g1Extractor

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
 const fs = require('fs-extra');
-const homofobia = require('./Racismo.json');
 
 
-async function takeData(termo){
+async function takeData(termo,pagina){
         let noticias = new Array();
-        for(let i=2007;i<2021;i++){    
-                for(let n = 1;n<36;n++){
+        //2007 é quando as primeiras noticias do g1 foram publicadas
+        for(let i=2007;i<new Date().getFullYear()+1;i++){
+            for(let n = 1;n<pagina;n++){
+                let termosQuebrados = termo.split(' ');
+                if(termosQuebrados.length >1){
+                    termo = termosQuebrados.join('+');
+                }
                 let url = `https://g1.globo.com/busca/?q=${termo}&order=recent&from=${i}-01-01T00%3A00%3A00-0300&to=${i}-12-31T23%3A59%3A59-0300&page=${n}&ajax=1`;
                 try{
-                let html =  await  axios.get(url);
-                const $ = cheerio.load(html.data);
-                if($('.widget--no-results').length){
+                    let html =  await  axios.get(url);
+                    const $ = cheerio.load(html.data);
+                    //se não houverem resultados, acabaram as noticias no ano então o loop é quebrado
+                    if($('.widget--no-results').length){
+                        break;
+                    }
+                    $('.widget--info__title').each((i,elem)=>{
+                        let tituloNoticia = elem.children[0].data;
+                        console.log(tituloNoticia);
+                        let url = $(elem).closest("a").attr('href');
+                        noticias.push({
+                            titulo:tituloNoticia.replace(/(\r\n|\n|\r|\\n)/gm, '').trim(),
+                            classificacao:'undefined',
+                            url
+                        });
+                    })
+                }
+                catch(e){
+                    //se houver um erro o loop é quebrado
+                    console.log(e);
                     break;
                 }
-                $('.widget--info__title').each((i,elem)=>{
-                    console.log(elem.children[0].data);
-                    noticias.push({titulo:elem.children[0].data.replace(/(\r\n|\n|\r|\\n)/gm, '').trim(),classificao:'undefined'});
-                })  
-            }catch(e){
-                console.log(e);
-                break;
+
             }
-            
         }
-    }
     await fs.writeJson(`${termo}.json`,noticias);
 
 
 }
 
-console.log(homofobia);
-
+takeData('travesti assassinada');
